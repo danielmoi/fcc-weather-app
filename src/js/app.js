8a0a37f470a778bebf901501f17a37f778304c8c
danielmoi <daniel@ampersandmoi.com>
2016-02-14 09:16:42 +1100

    Retrieve lat and lng from ipinfo

--- src/js/app.js
+++ src/js/app.js
@@ -3,33 +3,54 @@ function echo(obj) {
 }
 window.echo = console.log.bind(console);
 
+function dir(obj) {
+  console.dir(obj);
+}
+window.dir = console.dir.bind(console);
+
 var app = angular.module('myApp', []);
 
-app.factory('getIPdata', ['$http', function ($http, $scope) {
+app.factory('jsonIPinfo', ['$http', function ($http) {
   var obj = {};
-  obj.stuff = {};
-  obj.data = function () {
+  obj.data = {};
+  obj.getCity = function () {
     return $http.jsonp('http://ipinfo.io/json?callback=JSON_CALLBACK')
       .then(function (response) {
-      obj.stuff.city = response.data.city;
-        echo(obj.stuff.city);
-      
-//        angular.extend($scope.myData.data, response.data);
+        angular.extend(obj.data, response.data);
+        var comma = response.data.loc.indexOf(',');
+        obj.data.lat = response.data.loc.slice(0, comma);
+        obj.data.lng = response.data.loc.slice(comma + 1);
+        echo(obj.data);
+
       });
-    
+
   };
   return obj;
 }]);
 
+//app.factory('jsonWeather', function($http) {
+//  var obj = {};
+//  obj.getWeather = function(cityName) {
+//    return $http.jsonp('http://api.openweathermap.org/data/2.5/weather?lat=35.4&lon=139&appid=44db6a862fba0b067b1930da0d769e98')
+//    .then(function (response) {
+//      obj.weather = response.data;
+//      echo(obj.weather);
+//    });
+//    
+//  };
+//  return obj;
+//  
+//});
 
-app.controller('myController', ['getIPdata', function (getIPdata) {
-  
+app.controller('myController', ['$scope', 'jsonIPinfo', function ($scope, jsonIPinfo) {
+
   var vm = this;
 
+  angular.extend($scope, jsonIPinfo);
+  $scope.getCity();
+  console.dir($scope.data.city);
 
-  angular.extend(vm, getIPdata);
-  vm.data();
-  console.dir(vm.stuff.city);
+  //  $scope.getWeather($scope.data.city);
 
 
 
